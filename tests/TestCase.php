7dd628c695ee8ee5b82a43ebefbb8ef596a65238
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2026-07-29 17:13:12 +0000

    4 - PHPUint foi instalado e arquivos Pest (tests/...) foram substituídos pelos do PHPUnit.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    // O trait CreatesApplication é responsável por instanciar a aplicação
+    // Laravel para que os testes tenham acesso aos serviços e configurações.
+    use CreatesApplication;
 }
