142b10ea72a761c89b23b287e763c0c7fbd6ea10
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2026-07-29 19:48:07 +0000

    6 - Melhorias na migraçao e models com relação às relações entre tabelas de entidade user e group e o pivot group_user

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 #[Fillable(['manager_id', 'description'])]
 class Group extends Model
@@ -20,5 +21,25 @@ class Group extends Model
             'manager_id',    // 2. Chave estrangeira (FK) na tabela 'groups'
             'id'             // 3. Chave primária (Owner Key) na tabela 'users' (Opcional se for 'id')
         );
+    }
+
+
+    /**
+     * Relação de usuários pertencentes ao grupo.
+     * $group = Group::findOrFail(20);
+     * $group->users (Propriedade Dinâmica): Retorna a Coleção de usuários pronta para uso em memória.
+     * 
+     * $group->users() (Método/Query Builder): Retorna a Relação em si,
+     * permitindo encadear novos filtros SQL antes de buscar
+     * (ex: $group->users()->where('active', true)->get()).
+     * 
+     * ====== outro caso =======
+     */
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            User::class,     // 1. Model relacionada
+        )->withTimestamps();
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -32,7 +33,10 @@ class User extends Authenticatable
     }
 
     /**
-     * Os grupos que este usuário gerencia.
+     * $adm = User::(1);// retorna o usuário administrador.
+     * $adm->managedGroups;//retorna grupos administrados por ele.
+     * $adm->managedGroups();//retorna o objeto que representa a relação (HasMany).
+     * $adm->managedGroups()->get();//retorna grupos administrados por ele.
      */
     public function managedGroups(): HasMany
     {
@@ -41,5 +45,28 @@ class User extends Authenticatable
             'manager_id',   // 2. Chave estrangeira (Foreign Key) na tabela 'groups'
             'id'            // 3. Chave local (Local Key) na tabela 'users'
         );
+    }
+
+    /**
+     * $user->groups é diferente de $user->groups().
+     * o 1º tras os dados dos grupos relacionados ao usuários.
+     * o 2º tras a relação (o SQL code) e habilita novos processamentos em cima disso. Exemplos abaixo.
+     * 
+     * Relação de grupos aos quais o usuário pertence.
+     *
+     * $user->groups possui resultado igual a $user->groups()->get().
+     * 
+     * Associa o Usuário ID 1 ao Grupo ID 20
+     * $user->groups()->attach(20);
+     * 
+     * Se a pivot tiver campos adicionais (ex: 'status'), passe no segundo argumento:
+     * $user->groups()->attach(20, ['status' => 'active']);
+     * 
+     * recarregue a relação antes de solicitar o estado do objeto no mesmo processo, pois estará desatualizado:
+     * $user->load('groups')->groups
+     */
+    public function groups(): BelongsToMany
+    {
+        return $this->belongsToMany(Group::class)->withTimestamps();
     }
 }
